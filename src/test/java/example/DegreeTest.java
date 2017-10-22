b7a2841a27314b8f694dbf3e03f27367ae50a1e1
Sergey Khokhlov <sergey.a.khokhlov@gmail.com>
2017-10-22 14:14:26 +0300

    Fix error with DegreeTest

--- src/test/java/example/DegreeTest.java
+++ src/test/java/example/DegreeTest.java
@@ -8,15 +8,11 @@ import org.neo4j.harness.junit.Neo4jRule;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
-import static org.neo4j.driver.v1.Values.parameters;
 
 public class DegreeTest {
     // This rule starts a Neo4j instance
     @Rule
-    public Neo4jRule neo4j = new Neo4jRule()
-
-            // This is the function we want to test
-            .withFunction(Degree.class);
+    public Neo4jRule neo4j = new Neo4jRule().withProcedure(Degree.class);
 
     @Test
     public void shouldCalculateDegree() throws Throwable {
@@ -28,10 +24,9 @@ public class DegreeTest {
                 );
                 Session session = driver.session()
         ) {
-            String label = "User";
-            session.run("CREATE (p:User {name:'Brookreson'})", parameters("label", label));
+            session.run("CREATE (p:User {name:'Brookreson'})");
 
-            StatementResult result1 = session.run("CALL example.degree('{label}')", parameters("label", label));
+            StatementResult result1 = session.run("CALL example.degree('User')");
 
             assertThat(result1.single().get("degree").asInt(), equalTo(0));
         }
