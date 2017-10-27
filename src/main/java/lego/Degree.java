9088fe0833b61f1c2bef6b2a6a3e90711f93d7a8
Sergey Khokhlov <sergey.a.khokhlov@gmail.com>
2017-10-27 18:28:17 +0300

    Clean up

--- src/main/java/lego/Degree.java
+++ src/main/java/lego/Degree.java
@@ -2,21 +2,13 @@ package lego;
 
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Label;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.ResourceIterator;
-import org.neo4j.logging.Log;
 import org.neo4j.procedure.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Stream;
 
 public class Degree {
     @Context
     public GraphDatabaseService db;
-
-    @Context
-    public Log log;
 
     /**
      * Calculate degree of vertex
@@ -27,9 +19,7 @@ public class Degree {
     @Description("Calculate degree of vertex")
     public Stream<DegreeResults> degree(@Name("label") String label) {
         final Label ind = Label.label(label);
-        ResourceIterator<Node> nodes = db.findNodes(ind);
-
-        return nodes.stream().map((n) -> new DegreeResults(n.getId(), n.getDegree()));
+        return db.findNodes(ind).stream().map((n) -> new DegreeResults(n.getId(), n.getDegree()));
     }
 
     public class DegreeResults {
