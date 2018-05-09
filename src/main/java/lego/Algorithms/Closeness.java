0ae382d4875192074a722bb3f96eaf02162c35c7
Sergey Khokhlov <sergeyakhokhlov@gmail.com>
2018-05-09 12:48:05 +0300

    Closeness centrality speedup

--- src/main/java/lego/Algorithms/Closeness.java
+++ src/main/java/lego/Algorithms/Closeness.java
@@ -4,6 +4,8 @@ import lego.Direction;
 import lego.Graph;
 import lego.Results.CentralityResult;
 
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 
 public class Closeness {
@@ -52,6 +54,45 @@ public class Closeness {
      * @return Stream of {@link CentralityResult} with scores
      */
     public Stream<CentralityResult> getScores(Graph graph) {
-        return graph.getParallelVertexStream().map(vertex -> new CentralityResult(vertex, getVertexScore(graph, vertex)));
+//        return graph.getParallelVertexStream().map(vertex -> new CentralityResult(vertex, getVertexScore(graph, vertex)));
+        return calc(graph);
+    }
+
+    /**
+     * https://pdfs.semanticscholar.org/7d1b/c639258ad49af9e51199902313c3a2149c63.pdf
+     * @param graph
+     * @return
+     */
+    private Stream<CentralityResult> calc(Graph graph) {
+        final int verticesCount = graph.getVerticesCount();
+        ConcurrentHashMap<Integer, Double> map1 = new ConcurrentHashMap<>(verticesCount);
+
+        graph.getParallelVertexStream().forEach((source) -> {
+            Queue<Integer> queue = new LinkedList<>(); // Q
+            Map<Integer, Integer> distance = new HashMap<>(verticesCount); // d
+            Map<Integer, Integer> far = new HashMap<>(verticesCount);
+
+            queue.add(source);
+            distance.put(source, 0);
+            far.put(source, 0);
+
+            while (!queue.isEmpty()) {
+                int u = queue.poll();
+
+                for (int w : graph.getAdjacentVertices(u)) {
+                    if (!distance.containsKey(w)) {
+                        queue.add(w);
+                        distance.put(w, distance.get(u) + 1);
+                        far.put(source, far.get(source) + distance.get(w));
+                    }
+                }
+            }
+
+            map1.put(source, 1 / (double) far.get(source));
+        });
+
+        ArrayList<CentralityResult> res = new ArrayList<>(map1.size());
+        map1.forEach((integer, aDouble) -> res.add(new CentralityResult(integer, aDouble)));
+        return res.stream();
     }
 }
